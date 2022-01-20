b32bd97349416007f435a381bb6266d401a26960
Luis Trujillo <xultun18@gmail.com>
2022-01-19 21:00:53 -0500

    js

--- Semana 5/Día 1/codigo.js
+++ Semana 5/Día 1/codigo.js
@@ -1,3 +1,11 @@
-console.log("Hola mundo")
+let peso = 98;
+let estatura = 170;
 
-console.log(document.getElementsByTagName())
+if (peso < estatura); {
+    console.log ("podrá entrar a trabajar");
+}
+
+else (peso > estatura); {
+    console.log ("no podrá entrar a trabajar");
+}
+
